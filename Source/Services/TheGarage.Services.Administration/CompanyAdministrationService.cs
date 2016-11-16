78ba9481f55678b8eb3a3a55e561b463d694b7c1
N K <prvuser87@gmail.com>
2016-11-16 08:07:23 +0200

    Added Company CRUD service

---
+++ Source/Services/TheGarage.Services.Administration/CompanyAdministrationService.cs
@@ -0,0 +1,45 @@
+﻿namespace TheGarage.Services.Administration
+{
+    using System.Collections.Generic;
+
+    using TheGarage.Data;
+    using TheGarage.Data.Models;
+    using TheGarage.Services.Common.Administration;
+
+    public class CompanyAdministrationService : BaseAdministrationService, ICompanyAdministrationService
+    {
+        public CompanyAdministrationService(ITheGarageData data)
+            : base(data)
+        {
+        }
+
+        public void Create(Company entity)
+        {
+            this.Data.Companies.Add(entity);
+            this.Data.SaveChanges();
+        }
+
+        public void Delete(object id)
+        {
+            var content = this.Data.Companies.GetById(id);
+
+            this.Data.SaveChanges();
+        }
+
+        public Company Get(object id)
+        {
+            return this.Data.Companies.GetById(id);
+        }
+
+        public IEnumerable<Company> Read()
+        {
+            return this.Data.Companies.All();
+        }
+
+        public void Update(Company entity)
+        {
+            this.Data.Companies.Update(entity);
+            this.Data.SaveChanges();
+        }
+    }
+}
---
+++ Source/Services/TheGarage.Services.Common/Administration/ICompanyAdministrationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGarage.Data.Models;
+
+namespace TheGarage.Services.Common.Administration
+{
+    public interface ICompanyAdministrationService
+    {
+        void Create(Company entity);
+
+        void Delete(object id);
+
+        void Update(Company entity);
+
+        IEnumerable<Company> Read();
+
+        Company Get(object id);
+    }
+}
